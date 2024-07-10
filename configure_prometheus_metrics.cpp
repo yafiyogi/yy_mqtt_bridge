574523359e9830dd08978a04d6a70d5361ac1ce4
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2024-07-10 01:09:00 +0100

    configure_prometheus_metrics.cpp: use 'yy_data::make_lookup<>'. Use 'namespace prometheus'. Add label actions to 'Metric'

--- configure_prometheus_metrics.cpp
+++ configure_prometheus_metrics.cpp
@@ -30,68 +30,61 @@
 #include "spdlog/spdlog.h"
 #include "yaml-cpp/yaml.h"
 
-#include "yy_cpp/yy_flat_map.h"
 #include "yy_cpp/yy_flat_set.h"
+#include "yy_cpp/yy_make_lookup.h"
 #include "yy_cpp/yy_string_case.h"
 #include "yy_cpp/yy_string_util.h"
 #include "yy_cpp/yy_type_traits.h"
 #include "yy_cpp/yy_utility.h"
 #include "yy_cpp/yy_vector_util.h"
 
-#include "prometheus_config.h"
+#include "configure_prometheus_label_actions.h"
 #include "configure_prometheus_metrics.h"
-
+#include "prometheus_label_action.h"
 #include "mqtt_handler.h"
+#include "prometheus_config.h"
+#include "prometheus_metric.h"
+#include "yaml_util.h"
 
-namespace yafiyogi::mqtt_bridge {
-namespace prometheus_detail {
+namespace yafiyogi::mqtt_bridge::prometheus {
 namespace {
 
-static constexpr std::string_view g_type_guage{"gauge"};
+constexpr const std::string_view g_type_guage{"gauge"};
 
-struct metric_type_lookup final
-{
-    constexpr bool operator<(const metric_type_lookup & other) const noexcept
-    {
-      return name < other.name;
-    }
+constexpr const auto metric_types =
+  yy_data::make_lookup<std::string_view, MetricType>({{g_type_guage, MetricType::Guage}});
 
-    constexpr bool operator==(const metric_type_lookup & other) const noexcept
-    {
-      return name == other.name;
-    }
 
-    std::string_view name;
-    MetricType type;
-};
+enum class ActionType {Keep, ReplacePath};
+constexpr const std::string_view g_action_keep{"keep"};
+constexpr const std::string_view g_action_replace_path{"replace-path"};
+constexpr const std::string_view g_action_replace_path_1{"replacepath"};
+constexpr const std::string_view g_action_replace_path_2{"replace_path"};
 
-constexpr auto metric_types = yy_util::sort(yy_util::make_array(metric_type_lookup{g_type_guage, MetricType::Guage}));
-
-constexpr auto metric_type_comp = [](const metric_type_lookup & type,
-                                     const std::string & target) -> int
-{
-  return type.name.compare(target);
-};
-
-} // anonymous namespace
+constexpr const auto action_types =
+  yy_data::make_lookup<std::string_view, ActionType>({{g_action_keep, ActionType::Keep},
+                                                      {g_action_replace_path, ActionType::ReplacePath},
+                                                      {g_action_replace_path_1, ActionType::ReplacePath},
+                                                      {g_action_replace_path_2, ActionType::ReplacePath}});
 
 MetricType decode_metric_type(const std::string_view & p_metric_type_name)
 {
-  MetricType type = MetricType::Guage;
   const std::string metric_type_name{yy_util::to_lower(yy_util::trim(p_metric_type_name))};
 
-  if(auto [pos, found] = yy_util::find(metric_types,
-                                       metric_type_name,
-                                       metric_type_comp);
-     found)
-  {
-    type = pos->type;
-  }
-
-  return type;
+  return metric_types.lookup(metric_type_name, MetricType::Guage);
 }
 
-} // namespace prometheus_detail
+constexpr const std::string_view g_yaml_handlers{"handlers"};
+constexpr const std::string_view g_yaml_metric{"metric"};
+constexpr const std::string_view g_yaml_type{"type"};
+constexpr const std::string_view g_yaml_handler_id{"handler_id"};
+constexpr const std::string_view g_yaml_value{"value"};
+constexpr const std::string_view g_yaml_label_actions{"label_actions"};
+constexpr const std::string_view g_yaml_action{"action"};
+constexpr const std::string_view g_yaml_target{"target"};
+constexpr const std::string_view g_yaml_replace{"replace"};
+
+} // anonymous namespace
 
 MetricsMap configure_prometheus_metrics(const YAML::Node & yaml_metrics)
 {
@@ -101,53 +94,88 @@ MetricsMap configure_prometheus_metrics(const YAML::Node & yaml_metrics)
   {
     for(const auto & yaml_metric : yaml_metrics)
     {
-      auto & yaml_handlers = yaml_metric["handlers"];
+      auto & yaml_handlers = yaml_metric[g_yaml_handlers];
       yy_data::flat_set<std::string_view> handlers{};
       handlers.reserve(yaml_handlers.size());
 
-      auto metric_id = yy_util::trim(yaml_metric["metric"].as<std::string_view>());
+      auto metric_id = yy_util::trim(yaml_metric[g_yaml_metric].as<std::string_view>());
       spdlog::info(" Configuring Prometheus Metric [{}].",
-                    metric_id);
+                   metric_id);
       spdlog::debug("  [line {}].",
                     yaml_metric.Mark().line + 1);
-      MetricType type = prometheus_detail::decode_metric_type(yaml_metric["type"].as<std::string_view>());
+      MetricType type = decode_metric_type(yaml_metric[g_yaml_type].as<std::string_view>());
 
       for(const auto & yaml_handler : yaml_handlers)
       {
-        std::string_view handler_id{yy_util::trim(yaml_handler["handler_id"].as<std::string_view>())};
+        std::string_view handler_id{yy_util::trim(yaml_handler[g_yaml_handler_id].as<std::string_view>())};
         spdlog::info("   handler [{}]:", handler_id);
         spdlog::debug("    [line {}].", yaml_handler.Mark().line + 1);
 
         if(auto [ignore, emplaced] = handlers.emplace(handler_id);
            emplaced)
         {
-          MetricPtr metric;
-          if(auto & yaml_value = yaml_handler["value"];
-             yaml_value.IsScalar())
+          std::string property;
+
+          const auto & yaml_value = yaml_handler[g_yaml_value];
+          property = yaml_get_value(yaml_value, "");
+          if(!property.empty())
           {
-            std::string property = yaml_value.as<std::string>();
             spdlog::info("     - value [{}].", property);
             spdlog::debug("        [line {}].", yaml_value.Mark().line + 1);
-
-            metric = std::make_unique<Metric>(metric_id,
-                                              type,
-                                              std::move(property));
           }
 
-          if(metric)
+          const auto & yaml_label_actions = yaml_handler[g_yaml_label_actions];
+
+          LabelActions actions;
+          actions.reserve(yaml_label_actions.size());
+
+          for(const auto & yaml_label_action : yaml_label_actions)
           {
-            spdlog::info("     - add metric [{}] to handler [{}] property [{}].",
-                          metric->Id(),
-                          handler_id,
-                          metric->Property());
-            [[maybe_unused]]
-            auto [metrics_pos, ignore_found] = metrics.emplace(std::string{handler_id},
-                                                               Metrics{});
+            auto action_name = yy_util::to_lower(yy_util::trim(yaml_get_value(yaml_label_action[g_yaml_action], "")));
 
-            auto [ignore_key, handler_metrics] = metrics[metrics_pos];
+            spdlog::info("       - action [{}].", action_name);
+            spdlog::debug("          [line {}].", yaml_label_action.Mark().line + 1);
+            switch(action_types.lookup(action_name, ActionType::Keep))
+            {
+              case ActionType::ReplacePath:
+              {
+                std::string target{yy_util::trim(yaml_get_value(yaml_label_action[g_yaml_target], ""))};
 
-            handler_metrics.emplace_back(std::move(metric));
+                if(!target.empty())
+                {
+                  auto topics = configure_label_action_replace_path(yaml_label_action[g_yaml_replace]);
+
+                  auto action = yy_util::static_unique_cast<LabelAction>(std::make_unique<ReplacePathLabelAction>(std::move(target), std::move(topics)));
+
+                  actions.emplace_back(std::move(action));
+                }
+              }
+              break;
+
+              default:
+                spdlog::warn("Unrecognized action [{}]", action_name);
+                spdlog::debug("  [line {}].", yaml_label_action.Mark().line + 1);
+                break;
+            }
           }
+
+          auto metric = std::make_shared<Metric>(metric_id,
+                                                 type,
+                                                 std::move(property),
+                                                 std::move(actions));
+
+          spdlog::info("     - add metric [{}] to handler [{}] property [{}].",
+                       metric->Id(),
+                       handler_id,
+                       metric->Property());
+
+          [[maybe_unused]]
+          auto [metrics_pos, ignore_found] = metrics.emplace(std::string{handler_id},
+                                                             Metrics{});
+
+          auto [ignore_key, handler_metrics] = metrics[metrics_pos];
+
+          handler_metrics.emplace_back(std::move(metric));
         }
       }
     }
@@ -157,4 +185,4 @@ MetricsMap configure_prometheus_metrics(const YAML::Node & yaml_metrics)
 }
 
 
-} // namespace yafiyogi::mqtt_bridge
+} // namespace yafiyogi::mqtt_bridge::prometheus
